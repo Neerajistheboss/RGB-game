54b575f18dc9678b8000971243e88a02f9b0ce0b
Neerajistheboss <33841268+Neerajistheboss@users.noreply.github.com>
2019-09-15 01:12:21 +0530

    Update collector.cs

--- RGB/Assets/collector.cs
+++ RGB/Assets/collector.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class collector : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        Debug.Log("hello world");
+    }
+
     public void OnTriggerEnter2D(Collider2D col)
     {
         Destroy(col.gameObject);
